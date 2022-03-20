772f9ba540f6894cc28c15c0c68524d3e39ee0fa
mhdhfz <mhd.hfzz@gmail.com>
2022-03-20 11:26:13 +0800

    turbo boost with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,67 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
+        //different post, user
+        //Post::factory(5)->create();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
+        //same user different post
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-             'name' => 'Family',
-             'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-             'name' => 'Work',
-             'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'this post about my family',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
-             facere quidem porro commodi praesentium. Debitis odio et perferendis ea odit
-              reprehenderit consequuntur, voluptates magnam incidunt cum. Laboriosam 
-              perspiciatis temporibus illo repudiandae iusto dicta ipsum provident culpa? 
-              Adipisci dicta provident, consequuntur quasi cum sequi deserunt! Molestiae 
-              culpa eveniet quibusdam illum, corrupti repellat sunt vero officia temporibus 
-              error delectus, maiores vel reprehenderit neque soluta ipsum. Quaerat ipsa 
-              corrupti placeat provident minima maxime blanditiis qui vel. Aliquam provident 
-              porro ducimus dolorem quia vero neque, quod vel, eaque iste ipsum sint deleniti 
-              voluptatibus at possimus autem nihil iure. Excepturi fugit ut rerum obcaecati 
-              mollitia.'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'this post about my work',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
-             facere quidem porro commodi praesentium. Debitis odio et perferendis ea odit
-              reprehenderit consequuntur, voluptates magnam incidunt cum. Laboriosam 
-              perspiciatis temporibus illo repudiandae iusto dicta ipsum provident culpa? 
-              Adipisci dicta provident, consequuntur quasi cum sequi deserunt! Molestiae 
-              culpa eveniet quibusdam illum, corrupti repellat sunt vero officia temporibus 
-              error delectus, maiores vel reprehenderit neque soluta ipsum. Quaerat ipsa 
-              corrupti placeat provident minima maxime blanditiis qui vel. Aliquam provident 
-              porro ducimus dolorem quia vero neque, quod vel, eaque iste ipsum sint deleniti 
-              voluptatibus at possimus autem nihil iure. Excepturi fugit ut rerum obcaecati 
-              mollitia.'
-        ]);
-
-        
-
-
     }
 }
