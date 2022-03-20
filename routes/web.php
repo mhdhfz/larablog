d3f41d9d83df65b989b0e93d6e68ec63a3790276
mhdhfz <mhd.hfzz@gmail.com>
2022-03-20 11:43:45 +0800

    view all posts by an author

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File;
 
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
-     return view('posts', ['posts' => Post::with('category')->get()]);
+     return view('posts', ['posts' => Post::latest()->with('category', 'author')->get()]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -27,6 +28,10 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', ['posts' => $category->posts]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', ['posts' => $author->posts]);
 });
     
 
