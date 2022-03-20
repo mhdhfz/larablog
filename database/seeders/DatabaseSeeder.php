10fe2718640f79716b6c10421af0e9b4bbec5e71
mhdhfz <mhd.hfzz@gmail.com>
2022-03-20 11:01:44 +0800

    Database seeding saves time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,67 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+             'name' => 'Personal',
+             'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+             'name' => 'Family',
+             'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+             'name' => 'Work',
+             'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'this post about my family',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+             facere quidem porro commodi praesentium. Debitis odio et perferendis ea odit
+              reprehenderit consequuntur, voluptates magnam incidunt cum. Laboriosam 
+              perspiciatis temporibus illo repudiandae iusto dicta ipsum provident culpa? 
+              Adipisci dicta provident, consequuntur quasi cum sequi deserunt! Molestiae 
+              culpa eveniet quibusdam illum, corrupti repellat sunt vero officia temporibus 
+              error delectus, maiores vel reprehenderit neque soluta ipsum. Quaerat ipsa 
+              corrupti placeat provident minima maxime blanditiis qui vel. Aliquam provident 
+              porro ducimus dolorem quia vero neque, quod vel, eaque iste ipsum sint deleniti 
+              voluptatibus at possimus autem nihil iure. Excepturi fugit ut rerum obcaecati 
+              mollitia.'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'this post about my work',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+             facere quidem porro commodi praesentium. Debitis odio et perferendis ea odit
+              reprehenderit consequuntur, voluptates magnam incidunt cum. Laboriosam 
+              perspiciatis temporibus illo repudiandae iusto dicta ipsum provident culpa? 
+              Adipisci dicta provident, consequuntur quasi cum sequi deserunt! Molestiae 
+              culpa eveniet quibusdam illum, corrupti repellat sunt vero officia temporibus 
+              error delectus, maiores vel reprehenderit neque soluta ipsum. Quaerat ipsa 
+              corrupti placeat provident minima maxime blanditiis qui vel. Aliquam provident 
+              porro ducimus dolorem quia vero neque, quod vel, eaque iste ipsum sint deleniti 
+              voluptatibus at possimus autem nihil iure. Excepturi fugit ut rerum obcaecati 
+              mollitia.'
+        ]);
+
+        
+
+
     }
 }
