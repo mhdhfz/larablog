e89142a1a53c62199a3b710da10d3f0a6d4b968e
mhdhfz <mhd.hfzz@gmail.com>
2022-03-20 15:39:56 +0800

    a small js dropdown detour

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,10 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
-     return view('posts', ['posts' => Post::latest()->get()]);
+     return view('posts', [
+         'posts' => Post::latest()->get(),
+         'categories' => Category::all()
+    ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -27,11 +30,18 @@ Route::get('posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', ['posts' => $category->posts]);
+    return view('posts', [
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+    ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', ['posts' => $author->posts]);
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all()
+    ]);
 });
     
 
