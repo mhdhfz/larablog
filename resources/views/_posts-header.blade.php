dd27f145c627cd46fbdcc71b0feba3f73902c0c1
Hafiz Mustafa <mhd.hfzz@gmail.com>
2022-03-22 16:44:07 +0800

    Advanced eloquent query constraints

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -16,7 +16,7 @@
                 </x-slot>
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
                 @foreach ($categories as $category)
-                <x-dropdown-item href="/categories/{{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
+                <x-dropdown-item href="/?category={{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
                     <!-- :active="isset($currentCategory) && $currentCategory->is($category)"> -->
                     {{ ucwords($category->name) }}
                 </x-dropdown-item>
