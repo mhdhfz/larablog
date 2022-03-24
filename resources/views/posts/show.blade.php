026f9dfc5543fa947446bb0f20a47848e4109859
Hafiz Mustafa <mhd.hfzz@gmail.com>
2022-03-24 10:37:48 +0800

    markup for post comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -47,7 +47,15 @@
                         <p>{!! $post->body !!}</p>
                     </div>
                 </div>
+
+                {{-- comment --}}
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment />
+                    <x-post-comment />
+                </section>
             </article>
+
+
         </main>
     </section>
 </x-layout>
