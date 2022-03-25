c363e448524f2e999035fe638a2d39bae17a4d7a
Hafiz Mustafa <mhd.hfzz@gmail.com>
2022-03-25 09:48:51 +0800

    mailchimp api tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function(){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us14'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("8c9c6d7061", [
+        "email_address" => "someonetosubscribe@gmail.com",
+        "status" => "subscribed",
+    ]);
+
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
